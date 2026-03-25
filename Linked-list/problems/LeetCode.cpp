0bd2e0529271aaf7a46a083d2bff7d3cbc0603c7
Luiz Eduardo <lvieiradepaulo00@gmail.com>
2026-03-24 21:02:44 -0700

    linked-list/Add two numbers

--- Linked-list/problems/LeetCode.cpp
+++ Linked-list/problems/LeetCode.cpp
@@ -38,3 +38,27 @@ bool hasCycle(ListNode *head) {
     }
     return false;
 }
+
+// 2 Add Two Numbers
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    int carry = 0;
+    ListNode *newList= new ListNode(0), *tail=newList;
+    while(l1 || l2 || carry!=0){
+        int sum = carry;
+        if(l1){
+            sum+=l1->val;
+            l1=l1->next;
+        }
+        if(l2){
+            sum+=l2->val;
+            l2=l2->next;
+        }
+        ListNode *newNode = new ListNode(sum%10);
+        carry = sum/10;    
+        tail->next=newNode;
+        tail=tail->next;
+    }  
+    ListNode *answer = newList->next;
+    delete newList;
+    return answer; 
+}
